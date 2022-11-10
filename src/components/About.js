eb127a67f7c9ab5a5fbba9c363983ce50c7eed7d
Tonny Oseko <tonnyoseko0@gmail.com>
2022-11-10 12:05:30 +0300

    feat: About us styling

--- src/components/About.js
+++ src/components/About.js
@@ -2,23 +2,27 @@ import React from "react";
 
 function About() {
   return (
-    <div className="about" id="about">
+    <div className="container" id="about">
       <div className="about-content">
-        <h1>About Us</h1>
-        <p>
-          Akili Sawa is a mental health awareness and advocacy platform that
-          aims to promote mental health awareness and destigmatize mental health
-          issues in Kenya.
-        </p>
-        <p>
-          We are a group of young people who are passionate about mental health
-          and are committed to making a difference in the lives of young people
-          in Kenya.
-        </p>
-        <p>
-          We are committed to providing a safe space for young people to share
-          their experiences and learn from each other.
-        </p>
+        <h2>About Us</h2>
+        <div className="about-section">
+          <p>
+            Akili Sawa is a mental health awareness and advocacy platform that
+            aims to promote mental health awareness and destigmatize mental
+            health issues in Kenya.
+            <br />
+            <br />
+            We are a group of young people who are passionate about mental
+            health and are committed to making a difference in the lives of
+            young people in Kenya.
+            <br />
+            <br />
+            We are committed to providing a safe space for young people to share
+            their experiences and learn from each other.
+          </p>
+          {/* image section */}
+          
+        </div>
       </div>
     </div>
   );
