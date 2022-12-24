33e334cc497dd254cb8df35ce5c72f3ebda16bc1
Tonny Oseko <tonnyoseko0@gmail.com>
2022-12-24 12:52:26 +0300

    refactor changes in about us

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,36 @@
 import react from "react";
 
 function Contact() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Thank you for your message!");
+
+    window.location = "/";
+  };
   return (
-    <form className="contact-us">
-        <h2>Talk to Us</h2>
-        <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input type="email" className="form-control" id="email" placeholder="name@email.com" />
-        </div>
-        <div className="form-group">
-            <label htmlFor="message">Message</label>
-            <textarea className="form-control" id="message" rows="3" placeholder="How're you doing today..."></textarea>
-        </div>
-        <button type="submit" className="btn btn-dark">Submit</button>
+    <form className="contact-us" onSubmit={handleSubmit}>
+      <h2>Talk to Us</h2>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          placeholder="name@email.com"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="message">Message</label>
+        <textarea
+          className="form-control"
+          id="message"
+          rows="3"
+          placeholder="How're you doing today..."
+        ></textarea>
+      </div>
+      <button type="submit" className="btn btn-dark">
+        Submit
+      </button>
     </form>
   );
 }
